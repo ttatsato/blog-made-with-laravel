f39ad4df92839eb33d685a25cc87f495bdf4c85c
ttatsato <t-satoh@alby.co.jp>
2019-10-27 16:49:11 +0530

    first commit

---
+++ resources/js/parts/markdown.js
@@ -0,0 +1,29 @@
+// ここから追記する
+import marked from 'marked';
+
+// マークダウンをプレビュー画面に表示する
+$(function() {
+    marked.setOptions({
+        langPrefix: '',
+        breaks : true,
+        sanitize: true,
+    });
+
+    $('#markdown_editor_textarea').keyup(function() {
+        var html = marked(getHtml($(this).val()));
+        $('#markdown_preview').html(html);
+    });
+
+    // 個別の記事画面のマークダウンをHTMLに変換する
+    var target = $('.item-body')
+    var html = marked(getHtml(target.html()));
+    $('.item-body').html(html);
+
+    // 比較演算子が &lt; 等になるので置換
+    function getHtml(html) {
+        html = html.replace(/&lt;/g, '<');
+        html = html.replace(/&gt;/g, '>');
+        html = html.replace(/&amp;/g, '&');
+        return html;
+    }
+});
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+Route::get('/', 'Auth\PostController@showTopPage')->name('top');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/drafts/new', 'Auth\PostController@index')->name('drafts.new');
+Route::post('/drafts/new', 'Auth\PostController@postArticle')->name('drafts.new.posts');
+Route::get('/drafts/{id}', 'Auth\PostController@showArticle')->name('item');
